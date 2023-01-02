12b958f933e9df95770469c557d93d449963c1ba
SHOMRIDDHO'S WORLD <shomriddho.world@gmail.com>
2023-01-02 21:48:19 +0600

    Update 1035.py

--- cp1/1035.py
+++ cp1/1035.py
@@ -1,8 +1,9 @@
 a, b, c, d = map(int, input().split())
 
-if b > c and d > a and (c+d) > (a+b) and c > 0 and d > 0 and a % 2 == 0:
+if ((b > c) & (d > a )& ((c+d) > (a+b)) & (c >= 0) & (d >= 0) & (a % 2 == 0)):
     print("Valores aceitos")
 else:
     print("Valores nao aceitos")
 
 
+2+3*5+1
